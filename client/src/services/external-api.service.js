b021c16545958de4431ed2ec4696851f8e9cf10d
Nickolas <nickolasalcazar@gmail.com>
2023-04-15 10:42:44 -0700

    Made function more informative; console.logs and comments

--- client/src/services/external-api.service.js
+++ client/src/services/external-api.service.js
@@ -1,15 +1,28 @@
 import axios from "axios";
 
+/**
+ * Call external API at a URL specified in config parameter.
+ *
+ * @param {any} config
+ * @example
+ * const config = {
+ *  url: `${apiServerUrl}/api/messages/protected`,
+ *  method: "GET",
+ *  headers: {
+ *    "content-type": "application/json",
+ *    Authorization: `Bearer ${accessToken}`,
+ *  }
+ * const { data, error } = await callExternalApi(config);
+ */
 export const callExternalApi = async (config) => {
   return await axios
-    // .get(config.url, config.method, config.headers)
     .get(config.url, { headers: config.headers })
     .then((response) => {
-      console.log("response =", response.data);
+      console.log("callExternalApi():", response.data);
       return { data: response.data, error: undefined };
     })
     .catch((e) => {
-      console.log("Error =", e);
+      console.log("callExternalApi():", e);
       return { data: null, error: e };
     });
 };
