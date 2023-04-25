cfbd75b9c69135e67b80417207f625fef305feb1
Nickolas <nickolasalcazar@gmail.com>
2023-04-24 17:36:54 -0700

    Server can now read user data from frontend

--- server/index.js
+++ server/index.js
@@ -2,65 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-const helmet = require("helmet");
-const nocache = require("nocache");
-
-const { apiRouter } = require("./api/apiRouter");
-const { errorHandler } = require("./api/middleware/error.middleware");
-const { notFoundHandler } = require("./api/middleware/not-found.middleware");
+const { auth } = require("express-oauth2-jwt-bearer");
 
 dotenv.config();
-
-if (!(process.env.PORT && process.env.CLIENT_ORIGIN_URL)) {
-  throw new Error(
-    "Missing required environment variables. Check docs for more info."
-  );
-}
 
 const PORT = parseInt(process.env.PORT, 10);
 const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL;
 
-app.use(express.json());
-app.set("json spaces", 2);
-
-// Configure HTTP headers with Helmet
-app.use(
-  helmet({
-    hsts: {
-      maxAge: 31536000,
-    },
-    contentSecurityPolicy: {
-      useDefaults: false,
-      directives: {
-        "default-src": ["'none'"],
-        "frame-ancestors": ["'none'"],
-      },
-    },
-    frameguard: {
-      action: "deny",
-    },
-  })
-);
-
-app.use((req, res, next) => {
-  res.contentType("application/json; charset=utf-8");
-  next();
+const jwtCheck = auth({
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  audience: process.env.AUTH0_AUDIENCE,
+  tokenSigningAlg: "RS256",
 });
-app.use(nocache());
 
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN_URL,
-    methods: ["GET"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-    maxAge: 86400,
-  })
-);
+app.use(cors());
 
-app.use("/api", apiRouter);
+app.get("/api/messages/public", (req, res) => {
+  res.send("public resource");
+});
 
-app.use(errorHandler);
-app.use(notFoundHandler);
+app.get("/api/messages/protected", jwtCheck, async (req, res) => {
+  console.log("Accessed /api/messages/protected");
+
+  // Get the data of the logged in user who made the call
+  const payload = req.auth.payload;
+  console.log("req.auth.payload =", payload);
+
+  res.send("Hello, " + payload.name + " this is the backend!");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
