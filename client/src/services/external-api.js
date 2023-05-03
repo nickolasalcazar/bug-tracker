b6d81b546185bced81e32b6fb4223586b6b3ab05
Nickolas <nickolasalcazar@gmail.com>
2023-05-03 15:58:03 -0700

    Cleaned up function

--- client/src/services/external-api.js
+++ client/src/services/external-api.js
@@ -1,28 +1,26 @@
 import axios from "axios";
-
 /**
  * Call external API at a URL specified in config parameter.
  *
- * @param {any} config
  * @example
  * const config = {
- *  url: `${apiServerUrl}/api/messages/protected`,
- *  method: "GET",
- *  headers: {
- *    "content-type": "application/json",
- *    Authorization: `Bearer ${accessToken}`,
- *  }
+ *   url: `${apiServerUrl}/api/user/`,
+ *   method: "POST",
+ *   headers: {
+ *     "content-type": "application/json",
+ *     Authorization: `Bearer ${accessToken}`,
+ *   }
+ *   data: {
+ *     key1: "value1",
+ *   }
  * const { data, error } = await callExternalApi(config);
  */
-export const callExternalApi = async (config) => {
-  return await axios
-    .get(config.url, { headers: config.headers })
-    .then((response) => {
-      console.log("callExternalApi():", response.data);
-      return { data: response.data, error: undefined };
+export const callExternalApi = async ({ url, method, headers, data }) => {
+  return await axios({ url, method, headers: headers, data })
+    .then((res) => {
+      return { data: res.data, error: null };
     })
     .catch((e) => {
-      console.log("callExternalApi():", e);
       return { data: null, error: e };
     });
 };
