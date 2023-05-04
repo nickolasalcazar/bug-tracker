79b3ffb8ab914400f2e2055e94dae0f81203d338
Nickolas <nickolasalcazar@gmail.com>
2023-05-04 12:30:48 -0700

    Moved NavBar file; applied basic styling

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,42 +1,32 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-
 import { useAuth0 } from "@auth0/auth0-react";
 
-import ProfilePic from "./ProfilePic";
-import LoginButton from "./Login";
-import LogoutButton from "./Logout";
-import SignupButton from "./SignUp";
+import "./NavBar.css";
+
+import ProfilePic from "../ProfilePic";
+import LoginButton from "../Login";
+import LogoutButton from "../Logout";
+import SignupButton from "../SignUp";
 
 function NavBar() {
   // const user = useContext(UserContext);
   const { isAuthenticated, user } = useAuth0();
 
   return (
-    <nav>
-      <ul
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          listStyle: "none",
-        }}
-      >
-        <img
-          src="https://mugbug-public-assets.s3.us-west-1.amazonaws.com/logo-icon-v3.png"
-          style={{ width: "80px" }}
-        />
+    <nav className="nav-bar">
+      <ul>
         <li>
           <NavLink exact="true" to="/">
-            Home
+            <img
+              src="https://mugbug-public-assets.s3.us-west-1.amazonaws.com/logo-icon-v3-alt.png"
+              style={{ width: "50px" }}
+            />
           </NavLink>
         </li>
         <li>
           <NavLink to="/dashboard">Dashboard</NavLink>
         </li>
-        {/* <li>
-          <NavLink to="/protected-page">Protected Page</NavLink>
-        </li> */}
         {isAuthenticated ? (
           <>
             <li>
@@ -44,7 +34,7 @@ function NavBar() {
             </li>
             <li>
               <NavLink to="/profile">
-                <ProfilePic src={user.picture} />
+                <ProfilePic src={user.picture} alt={user.nickname} />
               </NavLink>
             </li>
           </>
@@ -53,7 +43,6 @@ function NavBar() {
             <li>
               <LoginButton />
             </li>
-
             <li>
               <SignupButton />
             </li>
