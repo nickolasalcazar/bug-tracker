8dd3aec5617f857420f1323862f21c74426fe707
Nickolas <nickolasalcazar@gmail.com>
2023-05-04 12:21:37 -0700

    Added predefined sizes to ProfilePic

--- client/src/components/ProfilePic.js
+++ client/src/components/ProfilePic.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 /**
+ * Renders a profile picture.
  *
  * @param {string} alt  Alt text.
- * @param {string} size Indicate size of rendered
+ * @param {string} size Indicate size of profile pic: [xs, sm, md, lg].
  * @param {string} src  URL of image.
  * @returns
  */
-export default function ProfilePic({ alt, size = "sm", src }) {
+export default function ProfilePic({ alt, size = "xs", src }) {
+  const sizes = {
+    xs: "25px",
+    sm: "50px",
+    md: "75px",
+    lg: "100px",
+  };
+
   return (
     <img
       alt={alt}
-      style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+      style={{ width: sizes[size], height: sizes[size], borderRadius: "50%" }}
       src={src}
       referrerPolicy="no-referrer"
     />
