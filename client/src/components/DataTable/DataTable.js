24ee9850edb6fd9e57bcd4b6bb79cc6dc5333bba
Nickolas <nickolasalcazar@gmail.com>
2023-05-19 12:27:38 -0700

    Made some adjustments to DataTable

--- client/src/components/DataTable/DataTable.js
+++ client/src/components/DataTable/DataTable.js
@@ -1,7 +1,46 @@
 import React from "react";
-
+import { createTheme, ThemeProvider } from "@mui/material";
 import MUIDataTable from "mui-datatables";
 
 export default function DataTable({ columns, rows }) {
-  return <MUIDataTable columns={columns} data={rows} />;
+  const theme = createTheme({
+    components: {
+      // MUIDataTableBodyCell: {
+      //   styleOverrides: {
+      //     root: {
+      //       backgroundColor: "#FF0000",
+      //     },
+      //   },
+      // },
+      MuiTableRow: {
+        styleOverrides: {
+          root: {
+            // backgroundColor: "#00FF00",
+            // height: "50px",
+            // height: 30,
+            // minHeight: "30px",
+            // maxHeight: "30px",
+          },
+        },
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <MUIDataTable
+        columns={columns}
+        data={rows}
+        options={{
+          responsive: "standard",
+          // responsive: "simple",
+          setTableProps: () => {
+            return {
+              size: "small",
+            };
+          },
+        }}
+      />
+    </ThemeProvider>
+  );
 }
