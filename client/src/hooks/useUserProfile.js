ed461a99c947377d3d13c5334f857cbbab9c9b8a
Nickolas <nickolasalcazar@gmail.com>
2023-06-12 15:04:16 -0700

    Added documentation

--- client/src/hooks/useUserProfile.js
+++ client/src/hooks/useUserProfile.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { getUserInfo } from "../services/user.api";
 
+/**
+ * Return user info belonging to the current signed-in user.
+ */
 export default function useUserProfile() {
   const { getAccessTokenSilently, user } = useAuth0();
   const [userProfile, setUserProfile] = useState(null);
