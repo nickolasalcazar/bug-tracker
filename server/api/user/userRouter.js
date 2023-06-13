f079c029fd27f41ce98df4cec78d18f84f97b4e4
Nickolas <nickolasalcazar@gmail.com>
2023-06-13 12:25:33 -0700

    Moved /connections paths above other paths

--- server/api/user/userRouter.js
+++ server/api/user/userRouter.js
@@ -5,15 +5,16 @@ const userRouter = Router();
 const controller = require("./controller");
 
 userRouter.use(jwtCheck);
-userRouter.get("/id/:id", controller.getUserById);
-userRouter.get("/:username", controller.getUserByUsername);
-userRouter.post("/", controller.createUser);
-// userRouter.delete("/:id", controller.deleteUser);
-// userRouter.put("/:id", controller.updateUser);
 
 // Connections
 userRouter.get("/connections/add/:id", controller.addConnection);
 userRouter.get("/connections/remove/:id", controller.removeConnection);
 userRouter.get("/connections/accept/:id", controller.acceptConnection);
+
+userRouter.get("/id/:id", controller.getUserById);
+userRouter.get("/:username", controller.getUserByUsername);
+userRouter.post("/", controller.createUser);
+// userRouter.delete("/:id", controller.deleteUser);
+// userRouter.put("/:id", controller.updateUser);
 
 module.exports = userRouter;
