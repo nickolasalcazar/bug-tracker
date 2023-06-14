6350d53882153e64a5a9fadc70eba93e523a3cec
Nickolas <nickolasalcazar@gmail.com>
2023-06-14 13:51:12 -0700

    Added query for selecting pending connections

--- server/api/notifs/queries.js
+++ server/api/notifs/queries.js
@@ -1,3 +1,15 @@
 module.exports = {
-  createTask: `SELECT NOW()`,
+  getPendingConnections: `
+    SELECT                                        
+      sender AS sender_id,
+      sender.username AS sender_username,
+      sender.nickname AS sender_nickname,
+      date_requested,
+      pending,
+      connected
+    FROM user_connections c
+    JOIN users u ON u.user_id = receiver
+    JOIN users sender ON sender.user_id = sender
+    WHERE receiver = 'oauth2|GCP-MugBug|111232945032492215545' AND u.user_id='oauth2|GCP-MugBug|111232945032492215545' AND pending = TRUE;
+  `,
 };
