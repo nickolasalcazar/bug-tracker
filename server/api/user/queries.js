67e1b94a3b07bf3624746aab9cd2ff9d4972813e
Nickolas <nickolasalcazar@gmail.com>
2023-06-15 14:29:24 -0700

    Added query getConnectionsByUsername

--- server/api/user/queries.js
+++ server/api/user/queries.js
@@ -21,12 +21,41 @@ module.exports = {
       nickname,
       picture
     FROM users
-    WHERE user_id IN (
+    WHERE user_id = $1 AND IN
       (SELECT sender FROM user_connections
-        WHERE receiver = $1 AND connected = TRUE)
+        WHERE receiver = users.user_id AND connected = TRUE)
       UNION
       (SELECT receiver FROM user_connections
-        WHERE sender = $1  AND connected = TRUE))`,
+        WHERE sender = users.user_id  AND connected = TRUE))`,
+
+  getConnectionsByUsername: `SELECT 
+    a AS user_id,
+    b AS username
+   FROM 
+      (SELECT
+        curr.user_id,
+        curr.username,
+        uc.sender,
+        uc.receiver,
+        other.user_id AS a,
+        other.username AS b
+      FROM users curr
+      JOIN user_connections uc ON
+        (sender = curr.user_id OR receiver = curr.user_id)
+      JOIN users other 
+        ON (uc.sender = other.user_id OR uc.receiver = other.user_id)
+      WHERE curr.username = $1)
+    AS derivedTable
+    WHERE b != $1`,
+  getConnectionsByUsername_Dev: `SELECT
+        other.user_id,
+        other.username
+      FROM users curr
+      JOIN user_connections uc ON
+        (sender = curr.user_id OR receiver = curr.user_id)
+      JOIN users other 
+        ON (uc.sender = other.user_id OR uc.receiver = other.user_id)
+      WHERE curr.username = $1 AND other.username != $1`,
   addConnection:
     "INSERT INTO user_connections(sender, receiver) VALUES ($1, $2)",
   removeConnection:
