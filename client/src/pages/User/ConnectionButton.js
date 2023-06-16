f4378c893b58e62158b3658ae50fcba41fd1c9e7
Nickolas <nickolasalcazar@gmail.com>
2023-06-16 16:57:32 -0700

    Added different buttons for different statuses

--- client/src/pages/User/ConnectionButton.js
+++ client/src/pages/User/ConnectionButton.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Chip } from "@mui/material";
+import { Box, Chip, Stack, Typography } from "@mui/material";
 import useConnections from "../../hooks/useConnections";
+
+import AddUserIcon from "@mui/icons-material/PersonAddRounded";
+import RemoveUserIcon from "@mui/icons-material/PersonRemoveRounded";
+import CancelIcon from "@mui/icons-material/CloseRounded";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 /**
  * A button for adding, accepting, or removing a connection request.
@@ -49,48 +54,119 @@ export default function ConnectionButton({ user, refresh }) {
     }
   };
 
-  // Add yser to components; React sees no change
-  if (loggedInUser.sub === user.user_id) return null;
-  else if (status === "not connected") {
+  const Connected = () => {
     return (
       <Chip
-        key={`${user.username} ${status}`}
+        label="Connected (Manage Connection)"
+        color="secondary"
+        clickable
+        onClick={handleRemove}
+      />
+    );
+  };
+
+  const NotConnected = () => {
+    return (
+      <Chip
         label="Connect"
         color="secondary"
         clickable
         onClick={handleAdd}
+        icon={<AddUserIcon />}
       />
     );
-  } else if (status === "connected") {
+  };
+
+  const SentRequest = () => {
     return (
       <Chip
-        key={`${user.username} ${status}`}
-        label="Connected"
-        color="secondary"
-        clickable
-        onClick={handleRemove}
-      />
-    );
-  } else if (status === "sent request") {
-    return (
-      <Chip
-        key={`${user.username} ${status}`}
         label="Cancel Request"
-        color="secondary"
         variant="outlined"
         clickable
         onClick={handleRemove}
+        icon={<CancelIcon />}
       />
     );
-  } else if (status === "received request") {
+  };
+
+  const ReceivedRequest = () => {
     return (
-      <Chip
-        key={`${user.username} ${status}`}
-        label="Received Connection (Accept)"
-        color="secondary"
-        clickable
-        onClick={handleAccept}
-      />
+      <Stack direction="column" spacing={1}>
+        <Typography variant="body2" textAlign="center">
+          Connection Request:
+        </Typography>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Chip
+            label="Decline"
+            variant="outlined"
+            color="primary"
+            clickable
+            onClick={handleRemove}
+            icon={<CancelIcon />}
+          />
+          <Chip
+            label="Accept Connection"
+            variant="contained"
+            color="secondary"
+            clickable
+            onClick={handleAccept}
+            icon={<AddUserIcon />}
+          />
+        </Stack>
+      </Stack>
     );
-  }
+  };
+
+  if (loggedInUser.sub === user.user_id) return null;
+  return (
+    <Box key={`${user.username} ${status}`}>
+      {status === "connected" ? <Connected /> : null}
+      {status === "not connected" ? <NotConnected /> : null}
+      {status === "sent request" ? <SentRequest /> : null}
+      {status === "received request" ? <ReceivedRequest /> : null}
+    </Box>
+  );
+
+  // if (status === "not connected") {
+  //   return (
+  //     <Chip
+  //       key={`${user.username} ${status}`}
+  //       label="Connect"
+  //       color="secondary"
+  //       clickable
+  //       onClick={handleAdd}
+  //     />
+  //   );
+  // } else if (status === "connected") {
+  //   return (
+  //     <Chip
+  //       key={`${user.username} ${status}`}
+  //       label="Connected"
+  //       color="secondary"
+  //       clickable
+  //       onClick={handleRemove}
+  //     />
+  //   );
+  // } else if (status === "sent request") {
+  //   return (
+  //     <Chip
+  //       key={`${user.username} ${status}`}
+  //       label="Cancel Request"
+  //       color="secondary"
+  //       variant="outlined"
+  //       clickable
+  //       onClick={handleRemove}
+  //     />
+  //   );
+  // } else if (status === "received request") {
+  //   return (
+  //     <Chip
+  //       key={`${user.username} ${status}`}
+  //       label="Received Connection (Accept)"
+  //       color="secondary"
+  //       clickable
+  //       onClick={handleAccept}
+  //     />
+  //   );
+  // }
 }
