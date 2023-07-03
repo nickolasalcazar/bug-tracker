7978fc9f25875475266e4d4b8930b854992a9221
Nickolas <nickolasalcazar@gmail.com>
2023-07-03 13:59:50 -0700

    Fixed height of PageLoader

--- client/src/components/PageLoader.js
+++ client/src/components/PageLoader.js
@@ -1,7 +1,7 @@
 import { Box, CircularProgress } from "@mui/material";
 export default function PageLoader() {
   return (
-    <Box display="flex" justifyContent="center" pt={25}>
+    <Box display="flex" justifyContent="center" py={10}>
       <CircularProgress size={80} />
     </Box>
   );
