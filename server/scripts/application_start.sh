b80d776da7fd025f132e7fc2582071cd18f42fef
Nickolas <nickolasalcazar@gmail.com>
2023-07-13 19:04:54 -0700

    Added script

--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Give permission for everything in the express-app directory
 sudo chmod -R /home/ec2-user/mugbug-app
 sudo chown -R ec2-user:ec2-user /home/ec2-user/mugbug-app
@@ -14,6 +13,13 @@ export NVM_DIR="$HOME/.nvm"
 
 # Install node modules
 npm install
+
+# Pull config variables
+echo AUTH0_API_ID=$(aws ssm get-parameter --name "/mugbug/auth0/api_id" | jq '.Parameter.Value') > .env
+echo AUTH0_AUDIENCE=$(aws ssm get-parameter --name "/mugbug/auth0/audience" | jq '.Parameter.Value') >> .env
+echo CLIENT_ORIGIN_URL=$(aws ssm get-parameter --name "/mugbug/auth0/client_origin_url" | jq '.Parameter.Value') >> .env
+echo AUTH0_DOMAIN=$(aws ssm get-parameter --name "/mugbug/auth0/domain" | jq '.Parameter.Value') >> .env
+echo AUTH0_SECRET=$(aws ssm get-parameter --name "/mugbug/auth0/secret" | jq '.Parameter.Value') >> .env
 
 # Start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
